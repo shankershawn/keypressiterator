25c6dc7c098380f485e47285584673145165fb0f
Shankarsan Ganai <shankershawn@gmail.com>
2019-06-26 23:55:12 +0530

    Added feature for key press and key release.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # keypressiterator
 
-The input of the program is A B C D ..... Y Z .. where A B C D are the KeyCodes to be executed. X is the number of iterations to be performed for the given KeyCode set. Z is the time interval in milliseconds between each keypress. It is a good practice to keeo the time interval within the range of 300-500.
+The input of the program is A B C D ..... Y Z .. where A B C D are the KeyCodes to be executed. X is the number of iterations to be performed for the given KeyCode set. Z is the time interval in milliseconds between each key execution. It is a good practice to keep the time interval within the range of 300-500.
 
-e.g. 9 9 9 32 100 400
+e.g. 9P 9R 9 32 100 400
 
-This means Tab key will be pressed three times followed by the space key once. The iteration count is 100 and 400 is the time gap between each keypress.
+Here P & R stand for Press and Release respectively. If P is used, the corresponding key is pressed and not released. Similarly, if R is used, the key is only released. If no param is used then the key will be pressed and released consecutively.
+
+This means here Tab key will be pressed, then released and then again pressed and released consecutively followed by the space key press and release. The iteration count is 100 and 400 is the time gap between each key execution.
--- src/main/java/com/shankarsan/keypressiterator/MainController.java
+++ src/main/java/com/shankarsan/keypressiterator/MainController.java
@@ -5,9 +5,10 @@ package com.shankarsan.keypressiterator;
 
 import java.awt.AWTException;
 import java.awt.Robot;
-import java.awt.event.KeyEvent;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.math.NumberUtils;
@@ -17,6 +18,9 @@ import org.apache.commons.lang3.math.NumberUtils;
  *
  */
 public class MainController {
+	
+	private static final String keyPressFlag = "P";
+	private static final String keyReleaseFlag = "R";
 
 	/**
 	 * @param args
@@ -25,25 +29,52 @@ public class MainController {
 		List<String> argList = null;
 		Robot keyPressRobot = null;
 		int sleepInterval = 0, iterations = 0;
-//		System.out.println(KeyEvent.VK_SPACE + " " + KeyEvent.VK_TAB);
+		String keyCode = null;
+		String keyCodeArg = null;
+		Set<Integer> pressedKeysList = null;
+//		System.out.println(KeyEvent.VK_WINDOWS + " " + KeyEvent.VK_TAB);
 		try {
-			if (ArrayUtils.isNotEmpty(args) && args.length > 1 && (argList = Arrays.asList(args)).stream().allMatch(arg -> NumberUtils.isParsable(arg))) {
+			if (ArrayUtils.isNotEmpty(args) && args.length > 1 &&
+					(argList = Arrays.asList(args))
+						.stream()
+						.allMatch(arg -> 
+							NumberUtils.isParsable(arg.split(keyPressFlag)[0]) || NumberUtils.isParsable(arg.split(keyReleaseFlag)[0])
+						)) {
 				sleepInterval = NumberUtils.toInt(argList.get(argList.size() - 1));
 				iterations = NumberUtils.toInt(argList.get(argList.size() - 2));
 				keyPressRobot = new Robot();
-				Thread.sleep(5000);
+				
+				Thread.sleep(3000);
 				while(iterations-- > 0) {
 					for(int index = 0; index < argList.size() - 2; index++) {
-						keyPressRobot.keyPress(NumberUtils.toInt(argList.get(index)));
-						keyPressRobot.keyRelease(NumberUtils.toInt(argList.get(index)));
+						keyCodeArg = argList.get(index);
+						keyCode = keyCodeArg.split(keyPressFlag)[0].split(keyReleaseFlag)[0];
+						if(keyCode.equals(keyCodeArg)) {
+							keyPressRobot.keyPress(NumberUtils.toInt(keyCode));
+							keyPressRobot.keyRelease(NumberUtils.toInt(keyCode));
+						}else if(keyCodeArg.contains(keyPressFlag)) {
+							keyPressRobot.keyPress(NumberUtils.toInt(keyCode));
+							if(null == pressedKeysList) {
+								pressedKeysList = new HashSet<>();
+							}
+							pressedKeysList.add(NumberUtils.toInt(keyCode));
+						}else if(keyCodeArg.contains(keyReleaseFlag)) {
+							keyPressRobot.keyRelease(NumberUtils.toInt(keyCode));
+						}
 						Thread.sleep(sleepInterval);
 					}
 				}
 			} else {
-				System.out.println("Invalid input!!! Please enter the list of key codes as integer, the iteration count as second last param and the interval in seconds as the last pararm!");
+				System.out.println("Invalid input!!! Please enter the list of key codes as integer, each integer followed by '!' if key is to be put on hold. The iteration count as second last param and the interval in seconds as the last pararm!");
 			}
 		} catch (AWTException | InterruptedException e) {
 			e.printStackTrace();
+		} finally {
+			if(null != pressedKeysList && null != keyPressRobot) {
+				for(Integer pressedKeyCode : pressedKeysList) {
+					keyPressRobot.keyRelease(pressedKeyCode);
+				}
+			}
 		}
 
 	}
